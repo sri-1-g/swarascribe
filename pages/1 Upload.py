3272cdc46007b5af6b7ecee254cb5b68a8b5c394
sri-1-g <harisripriya123@gmail.com>
2025-06-05 19:01:34 +0400

    Update 1 Upload.py

--- pages/1 Upload.py
+++ pages/1 Upload.py
@@ -14,7 +14,7 @@ st.sidebar.image("swara.png")
 col1, col2, col3 = st.columns(3)
 with col2:
     st.image("upload.png")
-st.write("Currently, the AI can only analyse the following ragams: Kharaharapriya, Shankarabharanam, Todi, Bhairavi, and Kalyani. Don't worry, more are to come!")
+st.write("Currently, the AI can analyse the following ragams: Kharaharapriya, Shankarabharanam, Todi, Bhairavi, and Kalyani. Don't worry, more are to come!")
 uploaded_file = st.file_uploader("Choose a file")
 audio_value = st.audio_input("Record a voice message")
 
