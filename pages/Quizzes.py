d3c56306a5eaa21fc55e2e49ed2a1f2aa60e92fe
sri-1-g <harisripriya123@gmail.com>
2025-07-15 00:58:46 +0400

    Update Quizzes.py

--- pages/Quizzes.py
+++ pages/Quizzes.py
@@ -4,9 +4,10 @@ import random
 st.set_page_config(page_title="Carnatic Raga Quizzes", layout="centered")
 
 st.title("🎵 Carnatic Music Raga Quizzes")
+st.write("Test your knowledge of Carnatic ragas with different types of quizzes!")
 
-quiz_type = st.sidebar.selectbox(
-    "Choose a Quiz Type:",
+quiz_type = st.radio(
+    "Select a Quiz Type:",
     [
         "1. Guess the Raga from Aro/Avo Audio",
         "2. Identify the Raga from Swaras",
